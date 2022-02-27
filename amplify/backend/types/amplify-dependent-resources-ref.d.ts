a09578ba36c3df0c5f9f8e9a656b3168f5e23513
noahmichel <noah_michel@yahoo.com>
2022-02-27 15:28:17 -0700

    Analytics and Header

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -22,5 +22,12 @@ export type AmplifyDependentResourcesAttributes = {
             "BucketName": "string",
             "Region": "string"
         }
+    },
+    "analytics": {
+        "sportsjournal": {
+            "Region": "string",
+            "Id": "string",
+            "appName": "string"
+        }
     }
 }
